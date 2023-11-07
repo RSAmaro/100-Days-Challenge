63578ef4135d74f5723266cd917ce852b43895e1
Amaro <37576434+RSAmaro@users.noreply.github.com>
2023-11-07 17:37:07 +0000

    day 17

---
+++ Day 17/example.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, user_id, user_name):
+        print("New User!")
+        self.id = user_id
+        self.username = user_name
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User("001", "Roberto")
+user_2 = User("002", "Maria")
+
+user_1.follow(user_2)
+print(user_1)
