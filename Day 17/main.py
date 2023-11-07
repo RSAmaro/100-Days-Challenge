dc8b332fda6a1daea0986acb0e89533d2dd95188
Amaro <37576434+RSAmaro@users.noreply.github.com>
2023-11-07 17:50:11 +0000

    For fun, decided to get questions from an API.

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,11 +1,12 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import Quiz
+import html
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = html.unescape(question["question"])
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
