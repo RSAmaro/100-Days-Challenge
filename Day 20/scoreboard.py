0abee32b67be93989fd48b3a48b2bee3f4561a3c
Amaro <37576434+RSAmaro@users.noreply.github.com>
2023-11-09 18:02:53 +0000

    Day 20 - Snake

---
+++ Day 20/scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+ALIGN = "center"
+FONT = ("Courier", 20, "bold")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color('white')
+        self.penup()
+        self.goto(0, 270)
+        self.update_score()
+        self.hideturtle()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
